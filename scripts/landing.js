166f80a0a2b070fda768ed383ecc0c334181cdc5
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2017-08-06 21:45:34 -0500

    Played with transitions + refactored function. Checkpoint8, assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,29 +2,15 @@ var animatePoints = function () {
 
     var points = document.getElementsByClassName('point');
 
-    var revealFirstPoint = function () {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.webkitTransform = "scaleX(1) translateY(0)";
-    };
+    var revealPoint = function (index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0) rotate(0deg)";
+        points[index].style.msTransform = "scaleX(1) translateY(0) rotate(0deg)";
+        points[index].style.webkitTransform = "scaleX(1) translateY(0) rotate(0deg)";
+    }
 
-    var revealSecondPoint = function () {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.webkitTransform = "scaleX(1) translateY(0)";
+    for (var counter=0; counter < points.length; counter++) {
+        revealPoint(counter);
     };
-
-    var revealThirdPoint = function () {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.webkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
 
 };
