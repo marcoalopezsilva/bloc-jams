9286c5c6a671544e4be619c269d2d4081ab333ef
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2017-08-21 20:55:14 -0500

    Replaced DOM scripting with JQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,33 @@
-var pointsArray = document.getElementsByClassName('point');
+// #5
+var animatePoints = function() {
 
-var animatePoints = function (points) {
-
-    var revealPoint = function (index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0) rotate(0deg)";
-        points[index].style.msTransform = "scaleX(1) translateY(0) rotate(0deg)";
-        points[index].style.webkitTransform = "scaleX(1) translateY(0) rotate(0deg)";
+    var revealPoint = function () {
+        // #7
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
     };
-
-    forEach( points, revealPoint);
-
+// #6
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function () {
-    // This next line triggers the animation if the user's window is too tall to permit scrolling. Otherwise, the animation would never be triggered;
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
+// The next block contains functions that need to be executed when an event occurs
+$(window).load(function() {
+
+    // #1
+    if ($(window).height() > 950) {
+        animatePoints();
     }
 
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    // #2
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-    window.addEventListener('scroll', function(event) {
-        //console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+    // #3
+    $(window).scroll(function(event) {
+        // #4
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
-    })
-};
+    });
+});
