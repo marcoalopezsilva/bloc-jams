38dd2404a3c1621a95cab27f201e041634f95c9f
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2017-08-24 22:29:17 -0500

    checkpoint18, try 1

--- scripts/album.js
+++ scripts/album.js
@@ -31,14 +31,57 @@ var albumMarconi = {
 };
 
 var createSongRow = function (songNumber, songName, songLength) {
+
     var template =
         '<tr class="album-view-song-item">'
     + '  <td class="song-item-number" data-song-number="'+ songNumber + '">' + songNumber + '</td>'
     + '  <td class="song-item-title">' + songName + '</td>'
     + '  <td class="song-item-duration">'+ songLength + '</td>'
-    + '</tr>'
-    ;
-    return $(template);
+    + '</tr>';
+
+    var $row = $(template);
+
+    var clickHandler = function () {
+        var songNumber = $(this).attr('data-song-number');
+        if (currentlyPlayingSong !== null) {
+            // Revert currently playing song to song number, because user started playing a new one
+//--> Check next line with Junior
+            var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
+            currentlyPlayingCell.html(currentlyPlayingSong);
+        }
+        if (currentlyPlayingSong !== songNumber) {
+            // Switch from Play to play-pause
+            $(this).html(pauseButtonTemplate);
+            currentlyPlayingSong = songNumber;
+        } else if (currentlyPlayingSong === songNumber) {
+            // Switch from Pause to Play
+            $(this).html(playButtonTemplate);
+            currentlyPlayingSong = null;
+        }
+    };
+
+    var onHover = function(event) {
+        var songNumberCell = $(this).find('.song-item-number');
+        var songNumber = songNumberCell.attr('data-song-number');
+        if (songNumber !== currentlyPlayingSong) {
+            songNumberCell.html(playButtonTemplate);
+        }
+    };
+
+    var offHover = function(event) {
+        var songNumberCell = $(this).find('.song-item-number');
+        var songNumber = songNumberCell.attr('data-song-number');
+        if (songNumber !== currentlyPlayingSong) {
+            songNumberCell.html(songNumber);
+        }
+    };
+
+    // Next line combines a callback (clickHandler) with finding the element with .song-item-number that triggers the click event
+    $row.find('.song-item-number').click(clickHandler);
+    // Next line handles what happens when the user hovers
+    $row.hover(onHover, offHover);
+    // #3
+    return $row;
 };
 
 var setCurrentAlbum = function (album) {
@@ -65,59 +108,6 @@ var setCurrentAlbum = function (album) {
     }
 };
 
-// Find the parent class of an element
-var findParentByClassName = function (element, targetClass) {
-    if (element) {
-        var currentParent = element.parentElement;
-        while (currentParent.className !== targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
-        }
-        return currentParent;
-    }
-};
-
-//Function which returns the song item
-// Note: I added breaks because I think they are missing from Bloc's code, but they don't seem to matter...
-var getSongItem = function (element) {
-    switch (element.className) {
-        case 'album-song-button':
-        case 'ion-play':
-        case 'ion-pause':
-            return findParentByClassName(element, 'song-item-number');
-        case 'album-view-song-item':
-            return element.querySelector('.song-item-number');
-        case 'song-item-title':
-        case 'song-item-duration':
-            // Note to self: WATCH FOR PERIODS WHEN USING querySelector!!!
-            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-        case 'song-item-number':
-            return element;
-        default:
-            return;
-    }
-};
-
-// This function determines what happens to the icon when the user clicks some song
-var clickHandler = function(targetElement) {
-    var songItem = getSongItem(targetElement);
-    if (currentlyPlayingSong === null) {
-        songItem.innerHTML = pauseButtonTemplate;
-        currentlyPlayingSong = songItem.getAttribute('data-song-number');
-    } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-        songItem.innerHTML = playButtonTemplate;
-        currentlyPlayingSong = null;
-    } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-        var currentlyPlayingSongElement = document.querySelector('[data-song-number= "' + currentlyPlayingSong + '"]');
-        currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-        songItem.innerHTML = pauseButtonTemplate;
-        currentlyPlayingSong = songItem.getAttribute('data-song-number');
-    }
-};
-
-//Create variables we can use for detecting when the user hovers over the song list
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
-
 // Add template for play button
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 // Add template for pause button
@@ -127,38 +117,6 @@ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause">
 var currentlyPlayingSong = null;
 
 // Add on-load functions
-window.onload = function () {
-        setCurrentAlbum(albumPicasso);
-
-      // Listen for the user hovering over the playlist, so we can change icons
-
-        // Register when the user hovers mouse over the song list container
-        songListContainer.addEventListener('mouseover', function (event) {
-            // Target only the line (song row) that the user is hovering on
-            if (event.target.parentElement.className === 'album-view-song-item') {
-                    var songItem = getSongItem(event.target);
-                    if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
-                        songItem.innerHTML = playButtonTemplate;
-                    }
-            }
-        });
-
-        // Listen for the user LEAVING the song row. Needs to be a for loop because each album has different number of songs!
-        for (var i = 0; i < songRows.length; i++) {
-            songRows[i].addEventListener('mouseleave', function (event) {
-                // #1
-                var songItem = getSongItem(event.target);
-                var songItemNumber = songItem.getAttribute('data-song-number');
-                // #2
-                if (songItemNumber !== currentlyPlayingSong) {
-                    songItem.innerHTML = songItemNumber;
-                }
-            });
-
-            // This next line registers a click that changes the value of currentlyPlayingSong
-            songRows[i].addEventListener('click', function (event) {
-                //Event handler call next
-                clickHandler(event.target);
-            });
-        }
-};
+$(document).ready(function() {
+    setCurrentAlbum(albumPicasso);
+});
